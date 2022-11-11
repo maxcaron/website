97115600fad3f0dad876ef639f8ee3fe1a5192f9
Maxime Caron <maxime@bloc.solutions>
2022-11-11 10:57:25 -0500

    test astro

--- static/js/app.js
+++
@@ -1,8 +0,0 @@
-import { sections } from './sections'
-export * from './about'
-
-export const activateSection = (sectionId) => sections.array.forEach(element =>
-  element === sectionId ?
-    document.getElementById(sectionId).className = 'ls-result active'
-    : document.getElementById(sectionId).className = 'ls-result'
-);
