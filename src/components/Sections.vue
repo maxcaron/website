8de98e185ac69bdaa918a5a0fbb52ae044f67e2f
Maxime Caron <maxime@bloc.solutions>
2022-11-11 11:40:39 -0500

    try vue dynamic component

---
+++ src/components/Sections.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex gap-4">
+        <span v-for="section in sections" 
+        class="hover:text-white cursor-pointer" 
+        :class="{'text-orange-400': section.title == selected?.title}"
+        @click="() => selectSection(section)"
+        >
+            {{section.title}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Sections',
+  data () {
+    return {
+      sections: [{title: 'Section 1'}, {title: 'Section 2'}],
+      selected: null
+    }
+  },
+  methods: {
+    selectSection (section) {
+      console.log('ici')
+      this.selected = section
+    }
+  }
+}
+</script>
+
+<style></style>
