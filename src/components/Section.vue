c755b549daf81f6246f710a032563d9859b40b86
Maxime Caron <maxime@bloc.solutions>
2022-11-11 12:23:39 -0500

    test dynamic hydration

---
+++ src/components/Section.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{title}}</span>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Section',
+  components: {},
+  props: {
+    title: String
+  },
+}
+</script>
+
+<style></style>
--- src/components/Sections.vue
+++ src/components/Sections.vue
@@ -1,22 +1,31 @@
 <template>
+    <div style="height: 4000px;"></div>
     <div class="flex gap-4">
         <span v-for="section in sections" 
         class="hover:text-white cursor-pointer" 
         :class="{'text-orange-400': section.title == selected?.title}"
-        @click="() => selectSection(section)"
         >
-            {{section.title}}
+          <Section client:visible :title="section.title" @click="() => selectSection(section)"/>
         </span>
+        <div>
+          {{response}}
+        </div>
     </div>
 </template>
 
 <script>
+import Section from './Section.vue'
+
 export default {
   name: 'Sections',
+  components: {
+    Section
+  },
   data () {
     return {
       sections: [{title: 'Section 1'}, {title: 'Section 2'}],
-      selected: null
+      selected: null,
+      response: null
     }
   },
   methods: {
@@ -24,6 +33,14 @@ export default {
       console.log('ici')
       this.selected = section
     }
+  },
+  async mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+      this.response = json.title
+      console.log(json)
+    })
   }
 }
 </script>
